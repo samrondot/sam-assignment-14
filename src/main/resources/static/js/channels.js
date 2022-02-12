973b448ecc63a9f076eba4cfd45432aa5fa5a6fd
samrondot <89955662+samrondot@users.noreply.github.com>
2022-02-12 16:33:05 -0500

    update

--- src/main/resources/static/js/channels.js
+++ src/main/resources/static/js/channels.js
@@ -2,21 +2,23 @@ var sendMessage = document.querySelector("#comment")
 var url = window.location.pathname;
 const queryString = window.location.href;
 var id = url.substring(url.lastIndexOf('/'))
-let channelId = queryString.substring(queryString.lastIndexOf("/"), queryString.length)
+let channelId = queryString.substring(queryString.lastIndexOf("/") +1, queryString.length)
 
-var user​ ​=​ ​JSON​.​parse​(​sessionStorage​.​getItem​("user")​)
+
+var user = JSON.parse(
+	sessionStorage.getItem("user"))
 
 console.log(user)
 
 sendMessage.addEventListener('keypress', function(e) {
 	if(e.key ==='Enter'){
-	
 	let newmessage = {
 		channelId: channelId,
 		message: sendMessage.value,
-		userId : userId,
-		username:username
+		userId : user.userId,
+		username: user.username
 	}
+	
 	sendMessage.value = ""
 	fetch('/messageSent'+id,{
 		method: 'POST',
@@ -26,7 +28,10 @@ sendMessage.addEventListener('keypress', function(e) {
 		body: JSON.stringify(newmessage)
 	})
 	.then((response) => response.json())
-	
-		}})
+		.then(message => {
+			console.log(message)
+		})}})
+		
+
 
 
